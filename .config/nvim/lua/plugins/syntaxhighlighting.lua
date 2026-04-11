326e6a823ad232a140440b6871bd3f2c6ab4deca
Brandon Burrus <brandon@burrus.io>
2026-04-10 18:20:08 -0700

    chore: update 2026-04-11T01:20:08.024Z

--- .config/nvim/lua/plugins/syntaxhighlighting.lua
+++ .config/nvim/lua/plugins/syntaxhighlighting.lua
@@ -6,32 +6,20 @@ return {
     'folke/todo-comments.nvim',
   },
   config = function()
-    -- v1 setup only accepts install_dir; all other options moved to native Neovim APIs
-    require('nvim-treesitter').setup()
-
-    local parsers = {
-      'lua', 'vim', 'vimdoc',
-      'elixir', 'javascript', 'typescript',
-      'html', 'css', 'python',
-      'json', 'toml', 'yaml', 'csv',
-      'dart', 'dockerfile',
-      'git_config', 'go', 'gomod', 'gosum',
-      'graphql', 'helm', 'java',
-      'markdown', 'scss', 'bash',
-      'gdscript', 'godot_resource', 'gdshader',
-    }
-
-    -- Install any parsers that are not yet present
-    require('nvim-treesitter').install(parsers)
-
-    -- Enable treesitter features per filetype via native Neovim APIs
-    vim.api.nvim_create_autocmd('FileType', {
-      callback = function()
-        local ok = pcall(vim.treesitter.start)
-        if ok then
-          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end
-      end,
+    require('nvim-treesitter.configs').setup({
+      ensure_installed = {
+        'lua', 'vim', 'vimdoc',
+        'elixir', 'javascript', 'typescript',
+        'html', 'css', 'python',
+        'json', 'toml', 'yaml', 'csv',
+        'dart', 'dockerfile',
+        'git_config', 'go', 'gomod', 'gosum',
+        'graphql', 'helm', 'java',
+        'markdown', 'scss', 'bash',
+        'gdscript', 'godot_resource', 'gdshader',
+      },
+      highlight = { enable = true },
+      indent = { enable = true },
     })
   end,
 }
